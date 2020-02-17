329fbd46eea5fdabb9529103f484e110f482d518
ritohirade <rittonboy89love@gmail.com>
2020-02-18 01:52:29 +0700

    bvjkner

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+    def new 
+    end
+    def create
+    end
+    def index
+    end
+    def show
+        @user = User.find(params[:id])
+    end
+    def edit
+    end
+    def destroy
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
   devise_for :users
   get 'top/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "top#index"
+
+  resources :users, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :products, only: [:index, :show]
 end
