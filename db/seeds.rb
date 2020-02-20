ddccff2a7690e4ceff9de8f57c1484d6df4b0e40
ritohirade <rittonboy89love@gmail.com>
2020-02-20 15:12:21 +0700

    bckejn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "Create Products"
+Product.create!(
+    brand_id: '1',
+    product_name: 'ニットワンピース＋チェックシャツセット',
+    product_image: open("#{Rails.root}/db/dummy_img/1.jpeg"),
+    price: 5230,
+)
+Product.create!(
+    brand_id: '2',
+    product_name: 'フリルハイネックトップス',
+    product_image: open("#{Rails.root}/db/dummy_img/1.jpeg"),
+    price: 3500
+)
+Product.create!(
+    brand_id: '3',
+    product_name: 'ビックボーダーセーター',
+    product_image: open("#{Rails.root}/db/dummy_img/1.jpeg"),
+    price: 4100,
+)
+Product.create!(
+    brand_id: '4',
+    product_name: 'ニットワンピース＋チェックシャツセット',
+    product_image: open("#{Rails.root}/db/dummy_img/1.jpeg"),
+    price: 5230,
+)
