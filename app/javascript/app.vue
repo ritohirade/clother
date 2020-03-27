e63aaabaab272a605adccc2c6bcff6e05317e535
ritohirade <rittonboy89love@gmail.com>
2020-03-27 17:16:05 +0900

    vagrant file create

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,34 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <button v-on:click="show" class="show1">
+      <i class="fas fa-chevron-up"></i>
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
+  data: function() {
     return {
       message: "Hello Vue!"
+    };
+  },
+  methods: {
+    show: function(event) {
+      alert("あーー");
     }
   }
-}
+};
 </script>
 
 <style scoped>
 p {
   font-size: 2em;
   text-align: center;
+}
+.show1 {
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
 }
 </style>
